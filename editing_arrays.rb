e1c6a6d945207c80bc771a8f27ef5c3428dfd1c1
wue19 <wue19@bcdschool.org>
2018-07-11 13:16:50 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,7 +5,7 @@ rainbow_colors = [
 ]
 
 def change_rainbow_colors
-  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+  rainbow_colors[]
 end
 
 def add_colors
